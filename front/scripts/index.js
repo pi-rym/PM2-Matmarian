67f960a542aae8bb7c9de346c3156318650880ff
Matmarian <matimariano85@gmail.com>
2024-05-14 17:29:59 -0300

    actualizado L3 M2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,49 +1,66 @@
-console.log(tempData);
-// Obtener el contenedor creado en el HTML
-const container = document.querySelector('.container');
+const moviesContainer = document.getElementById('movies-container');
 
-// Mapear los objetos en tempData y construir las tarjetas
-//tempData.forEach(movie); {
-    $.get("https://students-api.up.railway.app/movies", (movies)=>{
-        movies.forEach((movie) => {                  
 
-    // Crear elementos HTML para la tarjeta
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-    const personaje = document.createElement('div');
-    personaje.classList.add('personaje');
-
-    const imagenPelicula = document.createElement('div');
-    imagenPelicula.classList.add('imagen_pelicula');
-    imagenPelicula.style.backgroundImage = `url(${movie.poster})`; // Establecer la imagen de fondo
-
-    const detalle = document.createElement('div');
-    detalle.classList.add('detalle');
-
-    const title = document.createElement('h2');
-    title.textContent = movie.title;
-
-    const description = document.createElement('p');
-    description.textContent = `Year: ${movie.year}, Director: ${movie.director}, Duration: ${movie.duration}, Genre: ${movie.genre.join(', ')}, Rate: ${movie.rate}`;
-
-    const btn = document.createElement('div');
-    btn.classList.add('btn');
-    btn.innerHTML = '<h6>Leer Más</h6>';
-
-    // Construir la estructura de la tarjeta
-    detalle.appendChild(title);
-    detalle.appendChild(description);
-    detalle.appendChild(btn);
-
-    personaje.appendChild(imagenPelicula);
-    personaje.appendChild(detalle);
-
-    card.appendChild(personaje);
-
-    // Agregar la tarjeta al contenedor
-    container.appendChild(card);
-})
+$.get('https://students-api.up.railway.app/movies', (data) => {
+    // Verificar si la solicitud es exitosa
+    if (data && data.length > 0) {
+        // Renderizar las tarjetas de películas
+        renderCards(data);
+    } else {
+        console.error('No se recibió ningún dato de películas.');
+    }
 });
 
+// Función para renderizar las tarjetas de películas
+function renderCards(data) {
+    data.forEach((movie) => {
+        // Crear elementos HTML para la tarjeta
+        const card = document.createElement('div');
+        card.classList.add('card');
 
+        const title = document.createElement('h3');
+        title.classList.add('card-title');
+        title.textContent = `Título: ${movie.title}`;;
+
+        const director = document.createElement('p');
+        director.classList.add('card-director');
+        director.textContent = `Director: ${movie.director}`;;
+
+        const duration = document.createElement('p');
+        duration.classList.add('card-duration');
+        duration.textContent = `Duración: ${movie.duration}`;
+
+        const genre = document.createElement('p');
+        genre.classList.add('card-genre');
+        genre.textContent = `Genero: ${movie.genre}`;
+
+        const rate = document.createElement('p');
+        rate.classList.add('card-rate');
+        rate.textContent = `Puntuacion: ${movie.rate}`;
+               
+        const poster = document.createElement('img');
+        poster.classList.add('card-image');
+        poster.src = movie.poster;
+
+        const year = document.createElement('p');
+        year.classList.add('card-text');
+        year.textContent = `Año: ${movie.year}`;
+
+        const button = document.createElement('button');
+        button.classList.add('card-button');
+        button.textContent = 'Haz clic para Ver';
+
+        card.appendChild(poster);
+        card.appendChild(title);
+        card.appendChild(director);
+        card.appendChild(duration);
+        card.appendChild(genre);
+        card.appendChild(rate);
+        card.appendChild(year);
+        card.appendChild(button);
+     
+
+        // Agregar la tarjeta al contenedor
+        moviesContainer.appendChild(card);
+    });
+}
