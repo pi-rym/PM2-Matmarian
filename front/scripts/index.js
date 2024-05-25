86c36041c1f27c1d58233e2d44ef7ddb90b6597e
Matmarian <matimariano85@gmail.com>
2024-05-25 15:11:41 -0300

    actualizado L6 M2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,13 +1,14 @@
 
 const renderCards = require('./renderCards');
 
-$.get('https://students-api.up.railway.app/movies', (data) => {
-    // Verificar si la solicitud es exitosa
-    if (data && data.length > 0) {
-        // Renderizar las tarjetas de películas
-        renderCards(data);
-    } else {
-        console.error('No se recibió ningún dato de películas.');
+const fechtMovie = async ()=>{
+    try {
+        const data = await axios.get("https://students-api.up.railway.app/movies")
+        console.log(data.data);
+        
+    } catch (error) {
+        console.log(error.message);                
     }
-});
+}
 
+fechtMovie();
